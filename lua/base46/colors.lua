26f9e6be35a4f21a9d65ab3636e3c1143cec498e
Silzinc <jonasbloch854@proton.me>
2026-03-08 14:15:03 +0100

    feat: add functions for converting RGB to XYZ and CIECAM02 JCH colorspaces, and to harmonize the hue of a color with another one

--- lua/base46/colors.lua
+++ lua/base46/colors.lua
@@ -25,11 +25,18 @@
 
 local M = {}
 
--- Convert a hex color value to RGB
--- @param hex: The hex color value
--- @return r: Red (0-255)
--- @return g: Green (0-255)
--- @return b: Blue (0-255)
+---@param x number
+---@return integer
+local function signum(x)
+  if x < 0 then
+    return -1
+  elseif x == 0 then
+    return 0
+  else
+    return 1
+  end
+end
+
 ---Convert a hex color value to RGB
 ---@param hex string The hex color value
 ---@return number? r Red (0-255)
@@ -186,11 +193,193 @@ M.hsl2hex = function(h, s, l)
   return M.rgb2hex(r, g, b)
 end
 
--- Change the hue of a color by a given amount
--- @param hex The hex color value
--- @param amount The amount to change the hue.
---               Negative values decrease the hue, positive values increase it.
--- @return The hex color value
+---Convert a XYZ color value to RGB
+---@param x number
+---@param y number
+---@param z number
+---@return number r
+---@return number g
+---@return number b
+M.xyz2rgb = function(x, y, z)
+  local r, g, b =
+    3.240969941904523 * x - 1.537383177570094 * y - 0.498610760293003 * z,
+    -0.969243636280880 * x + 1.875967501507721 * y + 0.041555057407176 * z,
+    0.055630079696994 * x - 0.203976958888977 * y + 1.056971514242879 * z
+
+  r, g, b = math.min(math.max(r, 0.0), 1.0), math.min(math.max(g, 0.0), 1.0), math.min(math.max(b, 0.0), 1.0)
+
+  return math.floor(r * 255 + 0.5), math.floor(g * 255 + 0.5), math.floor(b * 255 + 0.5)
+end
+
+---Convert a RGB color value to XYZ
+---@param r number
+---@param g number
+---@param b number
+---@return number x
+---@return number y
+---@return number z
+M.rgb2xyz = function(r, g, b)
+  r, g, b = r / 255, g / 255, b / 255
+  return 0.412390799265959 * r + 0.357584339383878 * g + 0.180480788401834 * b,
+    0.212639005871510 * r + 0.715168678767756 * g + 0.072192315360734 * b,
+    0.019330818715592 * r + 0.119194779794626 * g + 0.950532152249661 * b
+end
+
+---Convert a JCH color value to XYZ with default viewing conditions as per material 3 design
+---@param j number
+---@param c number
+---@param h number
+---@return number x
+---@return number y
+---@return number z
+M.jch2xyz = function(j, c, h)
+  local alpha
+  if c == 0.0 or j == 0.0 then
+    alpha = 0.0
+  else
+    alpha = (c / math.sqrt(j / 100.0))
+  end
+
+  local t = (alpha / 0.8834525670408592) ^ (1.0 / 0.9)
+  local hRad = h * math.pi / 180.0
+  local eHue = 0.25 * (math.cos(hRad + 2.0) + 3.8)
+  local ac = 29.98099719444734 * ((j / 100.0) ^ 0.759112960023724)
+  local p1 = eHue * 3911.227617099522
+  local p2 = ac / 1.0169191804458757
+  local hSin = math.sin(hRad)
+  local hCos = math.cos(hRad)
+  local gamma = 23.0 * (p2 + 0.305) * t / (23.0 * p1 + 11 * t * hCos + 108.0 * t * hSin)
+  local a = gamma * hCos
+  local b = gamma * hSin
+
+  local rA = (460.0 * p2 + 451.0 * a + 288.0 * b) / 1403.0
+  local gA = (460.0 * p2 - 891.0 * a - 261.0 * b) / 1403.0
+  local bA = (460.0 * p2 - 220.0 * a - 6300.0 * b) / 1403.0
+
+  local rCBase = math.max(0, 27.13 * math.abs(rA) / (400.0 - math.abs(rA)))
+  local rC = signum(rA) / 0.003884814537800353 * (rCBase ^ (1.0 / 0.42))
+  local gCBase = math.max(0, 27.13 * math.abs(gA) / (400.0 - math.abs(gA)))
+  local gC = signum(gA) / 0.003884814537800353 * (gCBase ^ (1.0 / 0.42))
+  local bCBase = math.max(0, 27.13 * math.abs(bA) / (400.0 - math.abs(bA)))
+  local bC = signum(bA) / 0.003884814537800353 * (bCBase ^ (1.0 / 0.42))
+
+  local rF = rC / 1.02117770275752
+  local gF = gC / 0.9863077294280124
+  local bF = bC / 0.9339605082802299
+
+  local x = 1.86206786 * rF - 1.01125463 * gF + 0.14918677 * bF
+  local y = 0.38752654 * rF + 0.62144744 * gF - 0.00897398 * bF
+  local z = -0.01584150 * rF - 0.03412294 * gF + 1.04996444 * bF
+
+  return x, y, z
+end
+
+---Convert a XYZ color value to JCH with default viewing conditions as per material 3 design
+---@param x number
+---@param y number
+---@param z number
+---@return number j
+---@return number c
+---@return number h
+M.xyz2jch = function(x, y, z)
+  local rC = 0.401288 * x + 0.650173 * y - 0.051461 * z
+  local gC = -0.250268 * x + 1.204414 * y + 0.045854 * z
+  local bC = -0.002079 * x + 0.048952 * y + 0.953127 * z
+
+  local rD = 1.02117770275752 * rC
+  local gD = 0.9863077294280124 * gC
+  local bD = 0.9339605082802299 * bC
+
+  local rAF = (0.003884814537800353 * math.abs(rD)) ^ 0.42
+  local gAF = (0.003884814537800353 * math.abs(gD)) ^ 0.42
+  local bAF = (0.003884814537800353 * math.abs(bD)) ^ 0.42
+
+  local rA = signum(rD) * 400.0 * rAF / (rAF + 27.13)
+  local gA = signum(gD) * 400.0 * gAF / (gAF + 27.13)
+  local bA = signum(bD) * 400.0 * bAF / (bAF + 27.13)
+
+  local a = (11.0 * rA - 12.0 * gA + bA) / 11.0
+  local b = (rA + gA - 2.0 * bA) / 9.0
+  local u = (20.0 * rA + 20.0 * gA + 21.0 * bA) / 20.0
+  local p2 = (40.0 * rA + 20.0 * gA + bA) / 20.0
+
+  local atan2 = math.atan2(b, a)
+  local atanDegrees = atan2 * 180.0 / math.pi
+  local h = atanDegrees % 360
+
+  local ac = p2 * 1.0169191804458757
+
+  local j = 100.0 * ((ac / 29.98099719444734) ^ 1.3173270022537198)
+
+  local huePrime
+  if h < 20.14 then
+    huePrime = (h + 360)
+  else
+    huePrime = h
+  end
+  local eHue = 0.25 * (math.cos(huePrime * math.pi / 180.0 + 2.0) + 3.8)
+  local p1 = eHue * 3911.227617099522
+  local t = p1 * math.sqrt(a * a + b * b) / (u + 0.305)
+  local alpha = (t ^ 0.9) * 0.8834525670408592
+  local c = alpha * math.sqrt(j / 100.0)
+
+  return j, c, h
+end
+
+---Convert a hex color value to JCH with default viewing conditions as per material 3 design
+---@param hex string
+---@return number? j
+---@return number? c
+---@return number? h
+M.hex2jch = function(hex)
+  local r, g, b = M.hex2rgb(hex)
+  if not r or not g or not b then
+    return
+  end
+  local x, y, z = M.rgb2xyz(r, g, b)
+  return M.xyz2jch(x, y, z)
+end
+
+---Convert a JCH color value to hex with default viewing conditions as per material 3 design
+---@param j number
+---@param c number
+---@param h number
+---@return string hex
+M.jch2hex = function(j, c, h)
+  local x, y, z = M.jch2xyz(j, c, h)
+  local r, g, b = M.xyz2rgb(x, y, z)
+  return M.rgb2hex(r, g, b)
+end
+
+---Hue harominzation of a design color towards a source color. This uses the CIECAM02 JCH colorspace
+---instead of HSL: J holds the role of lightness, C that of chroma (somewhat related to saturation) and
+---H that of hue. This space is supposedly perceptually much more accurate than HSL and is better suited
+---for such manipulations.
+---@param design_color string Hex string
+---@param source_color string Hex string
+---@param harmony number (0 - 1) How much `design_color` must be shifted towards `source_color`
+---@param threshold number (0 - 180) Maximum authorized hue shift, in degrees
+---@return string? hex nil if one of the input hex strings is incorrect
+M.harmonize = function(design_color, source_color, harmony, threshold)
+  local j, c, from_h = M.hex2jch(design_color)
+  local _, _, to_h = M.hex2jch(source_color)
+  if not j or not c or not from_h or not to_h then
+    return
+  end
+
+  local diff_degrees = 180.0 - math.abs(math.abs(to_h - from_h) - 180.0)
+  local rot_degrees = math.min(diff_degrees * harmony, threshold)
+
+  local rotate_dir
+  if (to_h - from_h) % 360.0 < 180.0 then
+    rotate_dir = 1.0
+  else
+    rotate_dir = -1.0
+  end
+
+  local new_hue = (rot_degrees * rotate_dir + from_h) % 360.0
+  return M.jch2hex(j, c, new_hue)
+end
 
 ---Change the hue of a color by a given amount
 ---@param hex string The hex color value
